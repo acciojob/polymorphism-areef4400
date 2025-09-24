51eb19f99c7922c68c41d097d6058ca0daed28d0
areef4400 <mohammedareef35@gmail.com>
2025-09-24 23:41:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -13,8 +13,8 @@ class Product{
 public class Main {
     public static void main(String []args){
         Product p = new Product();
-        System.out.println(p.product(5, 6);
-        System.out.println(p.product(5, 6, 3);
-        System.out.println(p.product(5.5, 4.0);
+        System.out.println(p.product(5, 6));
+        System.out.println(p.product(5, 6, 3));
+        System.out.println(p.product(5.5, 4.0));
     }
 }
